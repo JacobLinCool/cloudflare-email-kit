30fb3b24792059db92aeaa31ff85cca877c25a34
JacobLinCool <jacoblincool@gmail.com>
2023-11-08 20:35:39 +0800

    chore: add license comment to mimetext re-export

--- packages/cloudflare-email/src/index.ts
+++ packages/cloudflare-email/src/index.ts
@@ -5,4 +5,6 @@ export * from "./middleware/size-guard";
 export * from "cloudflare-email-kit";
 export * from "cloudflare-email-mailchannels";
 export * from "cloudflare-email-router";
+
+/** @license MIT (https://github.com/muratgozel/MIMEText) */
 export * from "mimetext";
