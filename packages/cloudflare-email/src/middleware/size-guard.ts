ba90b4b2a957492726d059db8d4a6eeadee4dd71
JacobLinCool <jacoblincool@gmail.com>
2023-12-01 11:35:28 +0800

    docs: docstring of middleware size guard

--- packages/cloudflare-email/src/middleware/size-guard.ts
+++ packages/cloudflare-email/src/middleware/size-guard.ts
@@ -1,8 +1,15 @@
 import type { Context, Middleware } from "cloudflare-email-kit";
 
+/**
+ * Middleware that guards against messages exceeding a maximum size.
+ * If the message exceeds the maximum size, it will be rejected.
+ */
 export class SizeGuard implements Middleware {
 	name = "Size Guard";
 
+	/**
+	 * @param max_size The maximum size of a message in bytes.
+	 */
 	constructor(protected max_size: number) {}
 
 	async handle(ctx: Context, next: () => Promise<void>): Promise<void> {
